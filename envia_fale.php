0f3dfe1d29125456c4829e9d2f1b92dac8b97052
HalexandreLord <82584042+HalexandreLord@users.noreply.github.com>
2022-01-21 17:03:28 -0300

    Update envia_fale.php

--- envia_fale.php
+++ envia_fale.php
@@ -1,17 +1,22 @@
-<?php
+//enviar
 
-    $nome = $_POST['nome'];
-    $telefone = $_POST['telefone'];
-    $email = $_POST['email'];
-    $assunto = $_POST['assunto'];
-    $mensagem = $_POST['mensagem'];
+  // emails para quem será enviado o formulário
+  $emailenviar = "halexandrelordwilson@gmail.com";
+  $destino = $emailenviar;
+  $assunto = "Contato pelo Site";
 
-    $strcon = mysqli_connect('localhost','root','vertrigo','bancozem') or die('Erro ao conectar ao banco de dados');
-    $sql = "INSERT INTO contato VALUES ";
+  // É necessário indicar que o formato do e-mail é html
+  $headers  = 'MIME-Version: 1.0' . "\r\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+      $headers .= 'From: $nome <$email>';
+  //$headers .= "Bcc: $EmailPadrao\r\n";
 
-    $sql .= "('$nome', '$telefone', '$email', '$assunto', '$mensagem')"; 
-    mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
-    mysqli_close($strcon);
-
-    echo "Cliente cadastrado com sucesso!";
+  $enviaremail = mail($destino, $assunto, $arquivo, $headers);
+  if($enviaremail){
+  $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+  echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+  } else {
+  $mgm = "ERRO AO ENVIAR E-MAIL!";
+  echo "";
+  }
 ?>
