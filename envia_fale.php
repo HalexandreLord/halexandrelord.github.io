3b0fd9449e4f189db430c8a4d322e5ad46451022
HalexandreLord <82584042+HalexandreLord@users.noreply.github.com>
2022-01-21 16:45:41 -0300

    Update envia_fale.php

--- envia_fale.php
+++ envia_fale.php
@@ -1,29 +1,27 @@
-<?
+<?php
+//1 – Definimos Para quem vai ser enviado o email
+$para = "halexandrelordwilson@gmail.com";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$nome = $_POST['nome'];
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel
+//$assunto
+$assunto = $_POST['assunto'];
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$mensagem = "<strong>Nome:  </strong>".$nome;
+$mensagem .= "<br>  <strong>Mensagem: </strong>"
+.$_POST['mensagem'];
 
-# alterar a variavel abaixo colocando o seu email
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  dominio.com.br<sistema@dominio.com.br>\n";
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <sistema@dominio.com.br>\n";
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
 
-$destinatario = "halexandrelordwilson@gmail.com";
-
-$nome = $_REQUEST['nome'];
-$email = $_REQUEST['email'];
-$mensagem = $_REQUEST['mensagem'];
-$assunto = $_REQUEST['assunto'];
-
- // monta o e-mail na variavel $body
-
-$body = "===================================" . "\n";
-$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
-$body = $body . "===================================" . "\n\n";
-$body = $body . "Nome: " . $nome . "\n";
-$body = $body . "Email: " . $email . "\n";
-$body = $body . "Mensagem: " . $mensagem . "\n\n";
-$body = $body . "===================================" . "\n";
-
-// envia o email
-mail($destinatario, $assunto , $body, "From: $email\r\n");
-
-// redireciona para a página de obrigado
-header("location:obrigado.htm");
-
-
+mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
 ?>
