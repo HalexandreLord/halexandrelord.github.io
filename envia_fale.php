3ad2b31be37c9716e773bd18082348f39af50a66
HalexandreLord <82584042+HalexandreLord@users.noreply.github.com>
2022-01-21 16:56:18 -0300

    Update envia_fale.php

--- envia_fale.php
+++ envia_fale.php
@@ -1,27 +1,17 @@
 <?php
-//1 – Definimos Para quem vai ser enviado o email
-$para = "halexandrelordwilson@gmail.com";
-//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
-$nome = $_POST['nome'];
-// 3 - resgatar o assunto digitado no formulário e  grava na variavel
-//$assunto
-$assunto = $_POST['assunto'];
- //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
-$mensagem = "<strong>Nome:  </strong>".$nome;
-$mensagem .= "<br>  <strong>Mensagem: </strong>"
-.$_POST['mensagem'];
 
-//5 – agora inserimos as codificações corretas e  tudo mais.
-$headers =  "Content-Type:text/html; charset=UTF-8\n";
-$headers .= "From:  dominio.com.br<sistema@dominio.com.br>\n";
-//Vai ser //mostrado que  o email partiu deste email e seguido do nome
-$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
-//email do servidor //que enviou
-$headers .= "X-Mailer: PHP  v".phpversion()."\n";
-$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$headers .= "Return-Path:  <sistema@dominio.com.br>\n";
-//caso a msg //seja respondida vai para  este email.
-$headers .= "MIME-Version: 1.0\n";
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $assunto = $_POST['assunto'];
+    $mensagem = $_POST['mensagem'];
 
-mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
+    $strcon = mysqli_connect('localhost','root','vertrigo','bancozem') or die('Erro ao conectar ao banco de dados');
+    $sql = "INSERT INTO contato VALUES ";
+
+    $sql .= "('$nome', '$telefone', '$email', '$assunto', '$mensagem')"; 
+    mysqli_query($strcon,$sql) or die("Erro ao tentar cadastrar registro");
+    mysqli_close($strcon);
+
+    echo "Cliente cadastrado com sucesso!";
 ?>
