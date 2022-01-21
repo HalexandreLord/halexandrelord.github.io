2c47bae5b28f5e05615c85422a7e0100edf96871
HalexandreLord <82584042+HalexandreLord@users.noreply.github.com>
2022-01-21 16:34:59 -0300

    Update envia_fale.php

--- envia_fale.php
+++ envia_fale.php
@@ -2,7 +2,7 @@
 
 # alterar a variavel abaixo colocando o seu email
 
-$destinatario = "seuemail@seudominio.com.br";
+$destinatario = "halexandrelordwilson@gmail.com";
 
 $nome = $_REQUEST['nome'];
 $email = $_REQUEST['email'];
